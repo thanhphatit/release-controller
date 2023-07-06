ee6f51253bd9669d51f08005a4725980f46fc2de
root <thanhphatit95@gmail.com>
2023-07-06 14:29:40 +0000

    Update time: 20230706-022936

--- azure-release.sh
+++ azure-release.sh
@@ -358,7 +358,7 @@ function helm_deploy(){
             STATUS_PID_UPGRADE_HELM=$?
 
             if [[ "${STATUS_PID_UPGRADE_HELM}" == "0" ]];then
-                docker_deploy_latest 2> /dev/null
+                docker_deploy_latest &>/dev/null
             fi
         fi
 
