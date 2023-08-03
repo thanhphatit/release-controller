c64994d6ec3b52cb379be9b2ead16c03af7806c6
root <thanhphatit95@gmail.com>
2023-08-03 05:42:24 +0000

    Update time: 20230803-054220

--- azure-release.sh
+++ azure-release.sh
@@ -386,7 +386,7 @@ function helm_deploy(){
         if [[ ! "$(grep -i "${HELM_NAMESPACE_NAME}" ${LIST_HELM_RELEASE_K8S} | awk '{print $1}' | grep -i "^${HELM_RELEASE_NAME}$")" ]];then
             echo ""
             docker_deploy_latest 2>/dev/null
-            echo "[+] CHECKING: not found Helm Release [${HELM_RELEASE_NAME}] namespace [${HELM_NAMESPACE_NAME}]"
+            echo "##[warning][+] CHECKING: Not found Helm Release [${HELM_RELEASE_NAME}] namespace [${HELM_NAMESPACE_NAME}]"
             gitlab_status "pending"
             exit 1
         else
